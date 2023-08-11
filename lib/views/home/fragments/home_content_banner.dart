af4a7c6911d5123e3e82330b38ca955700c57bd7
Damai Subimawanto <damai.subimawanto@gmail.com>
2023-08-11 22:45:44 +0700

    Made pretty layout of banner list section in home_content_banner.dart page.

--- lib/views/home/fragments/home_content_banner.dart
+++ lib/views/home/fragments/home_content_banner.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:valbury_coding_test/models/banner_section_model.dart';
+import 'package:valbury_coding_test/utils/extensions/integer_extensions.dart';
 import 'package:valbury_coding_test/utils/extensions/string_extensions.dart';
 
 class HomeContentBanner extends StatelessWidget {
@@ -22,19 +23,28 @@ class HomeContentBanner extends StatelessWidget {
         itemBuilder: (BuildContext context, int position) {
           BannerModel bannerModel = model.bannerList![position];
 
-          return Card(
-            child: InkWell(
-              borderRadius: BorderRadius.circular(12.0),
-              onTap: () {
-                // TODO: Open promo page
-              },
-              child: Expanded(
-                child: Image.network(
-                  bannerModel.thumbnail.orEmpty(),
-                  fit: BoxFit.fitWidth,
+          return Row(
+            children: [
+              SizedBox(width: position == 0 ? 10.0 : 2.0),
+              Card(
+                clipBehavior: Clip.antiAlias,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(24.0),
+                  onTap: () {
+                    // TODO: Open promo page
+                  },
+                  child: Expanded(
+                    child: Image.network(
+                      bannerModel.thumbnail.orEmpty(),
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
                 ),
               ),
-            ),
+              SizedBox(
+                  width: position == (bannerCount.orZero() - 1) ? 10.0 : 2.0
+              )
+            ],
           );
         },
       ),
